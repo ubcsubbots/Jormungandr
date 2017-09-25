e548b5e3c4777f24d8210393a5f3f6961d48628b
Gareth Ellis <gareth.ellis0@gmail.com>
2017-09-24 20:59:20 -0700

    small fixes to the sample package

--- src/sample_package/src/MyNode.cpp
+++ src/sample_package/src/MyNode.cpp
@@ -19,8 +19,10 @@ MyClass::MyClass(int argc, char** argv, std::string node_name) {
     std::string parameter_name    = "my_node/character";
     std::string default_character = "!";
     if (!nh.getParam(parameter_name, suffix)){
-        ROS_ERROR_STREAM(nh.getNamespace() << ": no value given for " << parameter_name <<
-                                           " param, and no default value set!!");
+        ROS_INFO_STREAM(nh.getNamespace() 
+                << ": no value given for " << parameter_name
+                << ", using default value " << default_character);
+    
         suffix = default_character;
     }
 
