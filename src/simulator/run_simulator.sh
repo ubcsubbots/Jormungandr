fcc4c96a8c069b84dac51d17cde4dc434d3a273d
Cameron Newton <newtoncameron@hotmail.com>
2018-01-17 03:15:10 -0800

    Path changed in Qualification.xml, still a path error when running UWSim, not sure how to fix it.

--- src/simulator/run_simulator.sh
+++ src/simulator/run_simulator.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+
+
+# The current directory
+CURR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 # Check that we got exactly one argument
 if [ "$#" -ne 1 ]; then
     echo "This program takes 1 argument (the name of a scene in the scenes folder)."
