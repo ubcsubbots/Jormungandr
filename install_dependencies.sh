aaf94cd04c16b5e16a308cb7cff0792edc35c5c2
Vincent <yuanv@alumni.ubc.ca>
2018-02-07 12:11:10 -0800

    Comment .rosinstall as per #29 and added suggested .basrc setup

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -48,7 +48,38 @@ echo "================================================================"
 sudo mkdir -p /usr/share/ros/
 sudo chmod a+rwx /usr/share/ros
 rosinstall .
-echo "bash $CURR_DIR/setup.sh" >> ~/.bashrc
+
+
+echo "================================================================"
+echo "Setup .bashrc"
+echo "================================================================"
+# Shell config files that various shells source when they run.
+# This is where we want to add aliases, source ROS environment
+# variables, etc.
+SHELL_CONFIG_FILES=(
+    "$HOME/.bashrc"\
+            "$HOME/.zshrc"
+    )
+
+    # All lines listed here will be added to the shell config files
+    # listed above, if they are not present already
+    declare -a new_shell_config_lines=(
+        "source $CURR_DIR/setup.sh"
+        )
+
+    # Add all of our new shell config options to all the shell
+    # config files, but only if they don't already have them
+    for file_name in "${SHELL_CONFIG_FILES[@]}";
+    do
+        echo "Setting up $file_name"
+        for line in "${new_shell_config_lines[@]}";
+            do
+                if ! grep -Fq "$line" $file_name
+                then
+                    echo "$line" >> $file_name
+                fi
+            done
+    done
 
 echo "================================================================"
 echo "Installing Udev rules for phidgets"
