8cd204523c98a16e41ec8a18d461a19f50b3bdfc
Vincent Yuan <yuanv@alumni.ubc.ca>
2018-02-10 13:27:29 -0800

    Added phidget to rosinstall (#29)
    * adjusted the package installation format as done in Snowbots repo; This commit also includes installation of phidget libraries
    
    * Adjusted rosinstall files to include phidget drivers. Just need to run rosinstall.
    (Over-)Writing /home/dulluhan/Subbots/Jormungandr/.rosinstall
    [/usr/share/ros/uwsim] Updating /usr/share/ros/uwsim
    [/usr/share/ros/uwsim] Done.
    [/usr/share/ros/phidgets_api] Updating /usr/share/ros/phidgets_api
    [/usr/share/ros/phidgets_api] Done.
    (Over-)Writing setup.sh, setup.bash, and setup.zsh in /home/dulluhan/Subbots/Jormungandr
    
    rosinstall update complete.
    
    Now, type 'source /home/dulluhan/Subbots/Jormungandr/setup.bash' to set up your environment.
    Add that to the bottom of your ~/.bashrc to set it up every time.
    
    If you are not using bash please see http://www.ros.org/wiki/rosinstall/NonBashShells  on top level to setup
    
    * Comment .rosinstall as per #29 and added suggested .basrc setup
    
    * Fixed indent

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #######################################################################
-# STOP: If the dependency you want to add is required for the project # 
+# STOP: If the dependency you want to add is required for the project #
 #       to build, it should be added as a rosdep. This script should  #
 #       only contain other dependecies, like those required for gazebo#
 #######################################################################
@@ -36,7 +36,60 @@ echo "Installing Misc. Utilities"
 echo "================================================================"
 
 sudo apt-get install -y\
-    clang-format
+    clang-format\
+    python-rosinstall
+
+
+echo "================================================================"
+echo "Installing Project Dependent ROS packages."
+echo "================================================================"
+
+# Setup rosinstall
+sudo mkdir -p /usr/share/ros/
+sudo chmod a+rwx /usr/share/ros
+rosinstall .
+
+
+echo "================================================================"
+echo "Setup .bashrc"
+echo "================================================================"
+# Shell config files that various shells source when they run.
+# This is where we want to add aliases, source ROS environment
+# variables, etc.
+SHELL_CONFIG_FILES=(
+    "$HOME/.bashrc"\
+            "$HOME/.zshrc"
+    )
+
+# All lines listed here will be added to the shell config files
+# listed above, if they are not present already
+declare -a new_shell_config_lines=(
+    "source $CURR_DIR/setup.sh"
+    )
+
+# Add all of our new shell config options to all the shell
+# config files, but only if they don't already have them
+for file_name in "${SHELL_CONFIG_FILES[@]}";
+do
+    echo "Setting up $file_name"
+    for line in "${new_shell_config_lines[@]}";
+        do
+            if ! grep -Fq "$line" $file_name
+            then
+                echo "$line" >> $file_name
+            fi
+        done
+done
+
+echo "================================================================"
+echo "Installing Udev rules for phidgets"
+echo "================================================================"
+
+# Setup udev rules
+sudo cp /usr/share/ros/phidgets_api/share/udev/99-phidgets.rules /etc/udev/rules.d
+echo "Phidgets udev rules have been copied to /etc/udev/rules.d"
+# The reason the script isn't used is because stupid bash/sh doesn't know which directory it's in
+# The actual script that comes with phidget is under /usr/share/ros/phidgets_api/share/setup-udev.sh
 
 echo "================================================================"
 echo "Finished Installing Utilities"
