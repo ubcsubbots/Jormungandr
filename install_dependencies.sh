db49bb946498fcdbec484c55df717dbe675ca5be
Vincent <yuanv@alumni.ubc.ca>
2018-02-27 13:48:31 -0800

    Changed names and locations of installation file, also added directoty to gitignore

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -45,8 +45,8 @@ echo "Installing Project Dependent ROS packages."
 echo "================================================================"
 
 # Setup rosinstall
-mkdir -p extended_pkg
-rosinstall extended_pkg /opt/ros/kinetic .rosinstall
+mkdir -p external_pkg
+rosinstall external_pkg /opt/ros/kinetic .rosinstall
 rosinstall .
 
 echo "================================================================"
@@ -85,10 +85,12 @@ echo "Installing Udev rules for phidgets"
 echo "================================================================"
 
 # Setup udev rules
-sudo cp extended_pkg/phidgets_api/share/udev/99-phidgets.rules /etc/udev/rules.d
+# The reason the script that comes with the phidget package isn't used is
+# because stupid bash/sh doesn't know which directory it's in. The actual
+# script that comes with phidget is under external_pkg/phidgets_api/share/setup-udev.sh
+# and has more or less the same content as below:
+sudo cp external_pkg/phidgets_api/share/udev/99-phidgets.rules /etc/udev/rules.d
 echo "Phidgets udev rules have been copied to /etc/udev/rules.d"
-# The reason the script isn't used is because stupid bash/sh doesn't know which directory it's in
-# The actual script that comes with phidget is under /usr/share/ros/phidgets_api/share/setup-udev.sh
 
 echo "================================================================"
 echo "Finished Installing Utilities"
