bc7ddc787c05686a4577bce1e39dd7eb25bfd5fe
Vincent <yuanv@alumni.ubc.ca>
2018-02-26 20:06:38 -0800

    edited udev and .rosinstall locations

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -45,10 +45,9 @@ echo "Installing Project Dependent ROS packages."
 echo "================================================================"
 
 # Setup rosinstall
-sudo mkdir -p /usr/share/ros/
-sudo chmod a+rwx /usr/share/ros
-rosinstall /usr/share/ros /opt/ros/kinetic .rosinstall
-
+mkdir -p extended_pkg
+rosinstall extended_pkg /opt/ros/kinetic .rosinstall
+rosinstall .
 
 echo "================================================================"
 echo "Setup .bashrc"
@@ -86,7 +85,7 @@ echo "Installing Udev rules for phidgets"
 echo "================================================================"
 
 # Setup udev rules
-sudo cp /usr/share/ros/phidgets_api/share/udev/99-phidgets.rules /etc/udev/rules.d
+sudo cp extended_pkg/phidgets_api/share/udev/99-phidgets.rules /etc/udev/rules.d
 echo "Phidgets udev rules have been copied to /etc/udev/rules.d"
 # The reason the script isn't used is because stupid bash/sh doesn't know which directory it's in
 # The actual script that comes with phidget is under /usr/share/ros/phidgets_api/share/setup-udev.sh
