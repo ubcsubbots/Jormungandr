1a632d6c8bfdbf0a1fda0fb9c8eae876745ab3ba
reoliveira <oliveira.reid@gmail.com>
2018-03-24 13:19:08 -0700

    adjustments to subroutine.h

--- src/decision/subroutines/include/Subroutine.h
+++ src/decision/subroutines/include/Subroutine.h
@@ -13,18 +13,16 @@ class Subroutine {
 public:
     Subroutine(int argc, char **argv, std::string node_name) {
         ros::init(argc, argv, node_name);
-    };
+    }
 
     virtual void startup() = 0;
     virtual void shutdown() = 0;
-    virtual void publishCommand(const geometry_msgs::Twist::ConstPtr &msg) = 0;
+    void publishCommand(const geometry_msgs::Twist &msg) {
+        publisher_.publish(msg);
+    }
 
 protected:
-    std::vector<ros::Subscriber> subscriptions_;
     ros::Publisher publisher_;
-
-    virtual void setupSubscriptions(ros::NodeHandle nh) = 0;
-
 };
 
 #endif //DECISION_SUBROUTINE_H
