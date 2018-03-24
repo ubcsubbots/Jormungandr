f1840eb30f224ec28d72eee91922a636db013da6
Cameron Newton <newtoncameron@hotmail.com>
2018-03-24 12:55:47 -0700

    Updated with msgs

--- src/gate_detect/include/Gate.h
+++ src/gate_detect/include/Gate.h
@@ -9,34 +9,86 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <ros/ros.h>
 
-class Gate{
-    int maskWidth;
-    cv::Mat cdst, dst, matout;
-    int lowThreshold, lowVertThresh, lowHorThresh, poles, cannyLow, cannyHigh, poleLow, counter;
-    std::set<int> vertLines,vertPoles, horLines, horPoles;
+class Gate {
+    cv::Mat dst;
+    int lowThreshold, lowVertThresh, lowHorThresh, cannyLow, cannyHigh, poleMax, counter;
+    std::set<int> vertLines, vertPoles, horLines, horPoles;
+    std::vector<int> poleVector;
 
 public:
     Gate();
 
     /*
+     * Function to be called on cv::Mat to find vertical poles and horizontal poles
      *
+     * mat : cv::Mat potentially containing a gate
      *
      */
 
-    void initialize(const cv::Mat mat);
+    std::vector<int>  initialize(const cv::Mat mat);
+
+    /*
+     * Function to check if Gate object has been initialized
+     */
 
     bool checkMat();
 
 private:
 
     /*
+     * Function to filter through vector of cv::Vector4i objects and filter out the horizontal lines
      *
+     * allLines : Vector of cv::Vector4i objects in which the Vector4i represents a lines such that [xo , yo , xf, yf]
+     *
+     * xo = x coordinate of start of line
+     *
+     * yo = y coordinate of start of line
+     *
+     * xf = end coordinate of end of line
+     *
+     * yf = end coordinate of end of line
      *
      */
 
     std::set<int> filterHorLines(std::vector<cv::Vec4i> allLines);
 
+    /*
+     * Function to filter through vector of cv::Vector4i objects and filter out the Vertical lines
+     *
+     * allLines : Vector of cv::Vector4i objects in which the Vector4i represents a lines such that [xo , yo , xf, yf]
+     *
+     * xo = x coordinate of start of line
+     *
+     * yo = y coordinate of start of line
+     *
+     * xf = end coordinate of end of line
+     *
+     * yf = end coordinate of end of line
+     *
+     */
+
     std::set<int> filterVertLines(std::vector<cv::Vec4i> allLines);
+
+    /*
+    * Function to filter through set of horizontal lines to determine which lines constitute a pole
+    *
+    * int poleMax : maximum distance between two horizontal lines that function will call a pole
+    *
+    */
+
+    std::set<int> findHorPoles(std::set<int> horLines);
+
+    /*
+     * Function to filter through set of vertical lines to determine which lines constitute a pole
+     *
+     * int poleMax : maximum distance between two vertical lines that function will call a pole
+     *
+     */
+
+    std::set<int> findVertPoles(std::set<int> vertLines);
+
+    std::vector<int> gateVector(std::set<int> vertPoles, std::set<int> horPoles);
+
 
 };
 
--- src/gate_detect/src/Gate.cpp
+++ src/gate_detect/src/Gate.cpp
@@ -3,65 +3,38 @@
 
 Gate::Gate() {
     lowThreshold = 200;
-    lowVertThresh = 5;
-    lowHorThresh = 5;
-    cannyLow = 65;
-    cannyHigh = 250;
-    poleLow = 40;
-    counter = 0;
+    lowVertThresh = 5; //Sets the minimum deviation from start to finish that will constitute a vertical line
+    lowHorThresh = 5; //Sets the minimum deviation from start to finish that will constitute a horizontal line
+    cannyLow = 30; //Sets the low threshold for the canny filter, adjust if dst is not displaying all lines
+    cannyHigh = 250; //Sets the high threshold for the canny filter, adjust if dst not displaying all lines
+    poleMax = 60; //Sets the minimum threshold for two lines to be called a pole
+    counter = 0; //Resets counter
 }
 
-void Gate::initialize(const cv::Mat matin) {
+std::vector<int> Gate::initialize(const cv::Mat matin) {
     ROS_INFO("Initialize");
     cv::Canny(matin, dst, cannyLow, cannyHigh, 3);
 
     std::vector<cv::Vec4i> lines;
-    cv::HoughLinesP(dst, lines, 1, 0.5, 0, 10);
+    cv::HoughLinesP(dst, lines, 1, 1, 50, 50, 10);
 
-    if (counter == 0) {
-
-        imshow("dst",dst);
-        cvWaitKey(0);
+    if (counter == 10) {
 
         vertLines = filterVertLines(lines);
-        vertPoles.clear();
 
-        for (std::set<int>::iterator it1 = vertLines.begin(); it1 != vertLines.end(); it1++) {
-            cv::line(matin, cv::Point(*it1, 0), cv::Point(*it1, matin.rows), CV_RGB(255, 255, 255));
-        }
-
-        cv::imshow("Mat with lines", matin);
-        cvWaitKey(0);
-
-
-        //ROS_INFO("Rows %i Colums %i ", matin.rows , matin.cols);
-
-        for (std::set<int>::iterator it1 = vertLines.begin(); it1 != vertLines.end(); it1++) {
-
-            ROS_INFO("Iterator 1 %i", *it1);
-
-            for (std::set<int>::iterator it2 = vertLines.begin(); it2 != vertLines.end(); it2++) {
-
-                if ((*it1 != *it2) && ((*it1 - *it2) < poleLow) && (*it1 < matin.cols) && (*it2 < matin.cols)) {
-
-                    vertPoles.insert((*it1 + *it2) / 2);
-
-                }
-            }
-        }
-        /*
-        for (std::set<int>::iterator it1 = vertPoles.begin(); it1 != vertPoles.end(); it1++) {
-            ROS_INFO("Pole at %i", *it1);
-            cv::line(matin, cv::Point(*it1, 0),cv::Point(*it1,matin.rows),CV_RGB(255,255,255));
-        }
-        */
-        cv::imshow("Mat with lines", matin);
-        cvWaitKey(0);
-
+        vertPoles = findVertPoles(vertLines);
 
         horLines = filterHorLines(lines);
+
+        horPoles = findHorPoles(horLines);
+
         counter = 0;
+
     } else counter++;
+
+
+
+    return poleVector;
 
 }
 
@@ -71,14 +44,14 @@ bool Gate::checkMat() {
 
 std::set<int> Gate::filterVertLines(std::vector<cv::Vec4i> allLines) {
     std::set<int> vertLines;
+    vertLines.clear();
 
     for (int i = 0; i < (allLines.size() - 1); i++) {
 
         cv::Vec4i newLine = allLines[i];
 
         if (abs(newLine[0] - newLine[2]) < lowVertThresh) {
-            ROS_INFO("Vertical Line at %i", newLine[0]);
-            vertLines.insert(newLine[i]);
+            vertLines.insert((newLine[0] + newLine[2])/2);
         }
     }
 
@@ -86,17 +59,105 @@ std::set<int> Gate::filterVertLines(std::vector<cv::Vec4i> allLines) {
 }
 
 std::set<int> Gate::filterHorLines(std::vector<cv::Vec4i> allLines) {
-
     std::set<int> horLines;
+    horLines.clear();
 
     for (int i = 0; i < (allLines.size()); i++) {
 
         cv::Vec4i newLine = allLines[i];
 
-        if (abs(newLine[1] - newLine[3]) < lowVertThresh) {
+        if (abs(newLine[1] - newLine[3]) < lowHorThresh) {
             horLines.insert(newLine[i]);
         }
     }
 
     return horLines;
 }
+
+std::set<int> Gate::findVertPoles(std::set<int> vertLines){
+    std::set<int> vertPoles;
+    vertPoles.clear();
+
+    for (std::set<int>::iterator it1 = vertLines.begin(); it1 != vertLines.end(); it1++) {
+
+        for (std::set<int>::iterator it2 = vertLines.begin(); it2 != vertLines.end(); it2++) {
+
+            if ((*it1 != *it2) && (abs(*it1 - *it2) < poleMax)) {
+
+                vertPoles.insert((*it1 + *it2) / 2);
+
+            }
+        }
+    }
+
+    return vertPoles;
+
+}
+
+std::set<int> Gate::findHorPoles(std::set<int> horLines){
+    std::set<int> horPoles;
+    horPoles.clear();
+
+    for (std::set<int>::iterator it1 = horLines.begin(); it1 != horLines.end(); it1++) {
+
+        for (std::set<int>::iterator it2 = horLines.begin(); it2 != horLines.end(); it2++) {
+
+            if ((*it1 != *it2) && (abs(*it1 - *it2) < poleMax)) {
+
+                horPoles.insert((*it1 + *it2) / 2);
+
+            }
+        }
+    }
+
+    return  horPoles;
+
+}
+
+std::vector<int> gateVector(std::set<int> vertPoles, std::set<int> horPoles){
+
+    std::vector<int> gateCoord(9);
+
+    if (horPoles.size() > 1){
+
+        std::set::iterator it = horPoles.begin();
+        gateCoord[6] = *it;
+
+        for(*it ; *it != horPoles.end(); it++){
+
+            if (*it > gateCoord[6]){
+                gateCoord[6] = *it;
+                gateCoord[7] = 0;
+                gateCoord[8] = 0;
+            }
+        }
+    }
+    else if (horPoles.size() ==0) gateCoord[6] =0, gateCoord[7] = 9, gateCoord[8] = 0;
+    else gateCoord[6] = *horPoles.begin(), gateCoord[7] = 9, gateCoord[8] = 0;
+
+    if (vertPoles.size() > 2){
+
+        std::set::iterator it = vertPoles.begin();
+        gateCoord[6] = *it;
+
+        for(*it ; *it != vertPoles.end(); it++){
+
+            if (*it > gateCoord[6]){
+                gateCoord[6] = *it;
+            }
+        }
+    }
+    else if (horPoles.size() ==0) gateCoord[0] =0, gateCoord[1] = 9, gateCoord[2] = 0, gateCoord[3] =0, gateCoord[4] = 9, gateCoord[5] = 0;
+    else gateCoord[6] = *horPoles.begin(), gateCoord[7] = 9, gateCoord[8] = 0;
+
+    return gateCoord;
+
+}
+
+
+/*for (std::set<int>::iterator it1 = vertPoles.begin(); it1 != vertPoles.end(); it1++) {
+            cv::line(matin, cv::Point(*it1, 0),cv::Point(*it1,matin.rows),CV_RGB(255,255,255));
+        }
+
+        cv::imshow("Mat with lines", matin);
+        cvWaitKey(0);*/
