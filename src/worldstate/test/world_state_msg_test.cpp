03da80e5584e9151fa88ed0b24ad52a1535c3373
soledata32 <joel.ahn@alumni.ubc.ca>
2018-03-31 14:58:24 -0700

    Added test file

---
+++ src/worldstate/test/world_state_msg_test.cpp
@@ -0,0 +1,18 @@
+/*
+ *  Author: Joel
+ *  Date:  31/03/18
+ *  Purpose: Unit test to instantiate and message correctly outputs
+ */
+#include <ros/ros.h>
+#include <worldstate::
+
+int main(int argc, char** argv){
+    std::string node_name = "world_state";
+
+
+
+    ros::spin();
+
+    return 0;
+}
+
