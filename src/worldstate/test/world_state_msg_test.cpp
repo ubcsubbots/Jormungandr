8cd541b6f3b935d9760774fc0b5a3a532cd1aad6
soledata32 <joel.ahn@alumni.ubc.ca>
2018-04-07 11:20:55 -0700

    Created Instantiate Test

--- src/worldstate/test/world_state_msg_test.cpp
+++ src/worldstate/test/world_state_msg_test.cpp
@@ -3,15 +3,19 @@
  *  Date:  31/03/18
  *  Purpose: Unit test to instantiate and message correctly outputs
  */
-#include <ros/ros.h>
+#include <worldstate/state_msg.h>
+#include <gtest/gtest.h>
 
-int main(int argc, char** argv){
-    std::string node_name = "world_state";
+TEST(worldState, instantiateMsg) {
+    worldstate::state_msg msg;
 
+    msg.state = worldstate::state_msg<uint8_t>::locatingGate;
 
+    EXPECT_TRUE((msg.state == 0));
 
-    ros::spin();
-
-    return 0;
 }
 
+int main(int argc, char** argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
