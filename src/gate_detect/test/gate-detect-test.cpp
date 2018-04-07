6c44ab3b957aa6c321c2302830a6b58d9f9419b7
Cameron Newton <newtoncameron@hotmail.com>
2018-04-07 10:08:39 -0700

    Reformatted

--- src/gate_detect/test/gate-detect-test.cpp
+++ src/gate_detect/test/gate-detect-test.cpp
@@ -1,5 +1,5 @@
 //
-// Created by da-cam on 03/04/18.
+// Created by Cameron on 03/04/18.
 //
 
 #include <gate_detect/gateDetectMsg.h>
@@ -8,15 +8,15 @@
 TEST(gate_detect, addMessage) {
     gate_detect::gateDetectMsg msg;
 
-    msg.detectLeft = 1;
-    msg.angleLeft = 2.0f;
-    msg.distanceLeft = 2.0f;
-    msg.detectRight = 1;
-    msg.angleRight = 2.0f;
+    msg.detectLeft    = 1;
+    msg.angleLeft     = 2.0f;
+    msg.distanceLeft  = 2.0f;
+    msg.detectRight   = 1;
+    msg.angleRight    = 2.0f;
     msg.distanceRight = 2.0f;
-    msg.detectTop = 1;
-    msg.angleTop = 2.0f;
-    msg.distanceTop = 2.0f;
+    msg.detectTop     = 1;
+    msg.angleTop      = 2.0f;
+    msg.distanceTop   = 2.0f;
 
     EXPECT_TRUE(true);
 }
