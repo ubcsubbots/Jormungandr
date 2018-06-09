fe54a8eb6003075b767dcc3bd8f5e401052d86a1
soledata32 <joel.ahn@alumni.ubc.ca>
2018-06-09 14:07:30 -0700

    More CLang Formats

--- src/decision/src/subroutines/gate/LineUpWithGate.cpp
+++ src/decision/src/subroutines/gate/LineUpWithGate.cpp
@@ -38,7 +38,8 @@ const gate_detect::gateDetectMsg::ConstPtr& msg) {
         y_linear = LEFT;
     }
 
-    if (!(std::abs(msg->distanceTop * sin(msg->angleTop)) > subbots::global_constants::CLEARANCE_HEIGHT &&
+    if (!(std::abs(msg->distanceTop * sin(msg->angleTop)) >
+          subbots::global_constants::CLEARANCE_HEIGHT &&
           msg->angleTop < 0)) {
         z_linear = DOWN;
     }
