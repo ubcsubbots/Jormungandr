65c3dee03d3e1648a0b38d4ffe870916fdf313d9
viral28 <viral_28@live.com>
2018-07-18 22:42:42 -0700

    needs the intergrators and constant for the torque to pwm

--- src/controller/src/ControllerNode.cpp
+++ src/controller/src/ControllerNode.cpp
@@ -10,15 +10,23 @@
 #include "nav_msgs/Odometry.h"
 
 #include <Eigen/Dense> //matrix manipulation library
+#include "sensor_msgs/Imu.h"
 
 
 class ControllerNode {
 private:
   ros::Subscriber twist_sub;
   ros:: Publisher  arduino_pub;
+  ros::Subscriber IMU_sub;
   ros::NodeHandle nh;
 
+
 public:
+  Eigen::MatrixXd IMUangularvelocity;
+  Eigen::MatrixXd IMUlinearaccelaration;
+  Eigen::MatrixXd cumilativeIMUangularvelocity;
+  Eigen::MatrixXd cumilativeIMUlinearaccelaration;
+  Eigen::MatrixXd previousIMUlinearaccelaration;
   ControllerNode (std::string name){
     //initializes node
 
@@ -26,28 +34,40 @@ public:
 
     //Can the same node subscribe and publish?
     twist_sub = nh.subscribe("velocity_publisher", 1000, &ControllerNode::DesiredvelocityCallback, this);
+    IMU_sub =  nh.subscribe("imu_data", 1000, &ControllerNode::IMUCallback, this);
     arduino_pub = nh.advertise<std_msgs::Int32MultiArray>("Arduino",1000);
+
+
   }
   
   ~ControllerNode(void){};
-    
+  void IMUCallback  (const sensor_msgs::Imu::ConstPtr& msg){
+
+    IMUangularvelocity <<
+      msg->angular_velocity.x,
+      msg->angular_velocity.y,
+      msg->angular_velocity.z;
+    IMUlinearaccelaration << msg->linear_acceleration.x,
+      msg->linear_acceleration.y,
+      msg->linear_acceleration.z;
+  };
   void DesiredvelocityCallback(const  nav_msgs::Odometry::ConstPtr& desired_twist_velocity)
   {
 
-      Eigen::MatrixXd MatrixCurrentvelocity(6,1);
-      Eigen::MatrixXd MatrixDesiredvelocity(6,1);
+    Eigen::MatrixXd MatrixCurrentvelocity(6,1);
+    Eigen::MatrixXd MatrixDesiredvelocity(6,1);
     MatrixDesiredvelocity <<
-            desired_twist_velocity->twist.twist.linear.x,
-            desired_twist_velocity->twist.twist.linear.y,
-            desired_twist_velocity->pose.pose.position.z,//position in real, not velocity
-            desired_twist_velocity->twist.twist.angular.x,
-            desired_twist_velocity->twist.twist.angular.y,
-            desired_twist_velocity->twist.twist.angular.z;
+      desired_twist_velocity->twist.twist.linear.x,
+      desired_twist_velocity->twist.twist.linear.y,
+      desired_twist_velocity->pose.pose.position.z,//position in real, not velocity
+      desired_twist_velocity->twist.twist.angular.x,
+      desired_twist_velocity->twist.twist.angular.y,
+      desired_twist_velocity->twist.twist.angular.z;
 
 
     // Actual velocity to be got from the IMU and depth to be got from the pressure/depth sensor
 
-      MatrixCurrentvelocity <<
+    MatrixCurrentvelocity <<
       1,
       2,
       3,//position, not velocity
@@ -60,14 +80,16 @@ public:
     Eigen::MatrixXd K(4,6);
 
     K <<
-               0.3336,   -0.1862,   -0.4019,   -0.1243,    0.0020,    0.1623,
-               0.2801,    0.2301,    0.4458,    0.1266,   -0.0017,   -0.1713,
-              -0.0089,    0.1006,   14.5461,    0.1070,   -0.0016,    0.3206,
-               0.0102,   -0.1144,   13.9227,    0.8343,    0.0004,   2-0.3244;
+      0.3336,   -0.1862,   -0.4019,   -0.1243,    0.0020,    0.1623,
+      0.2801,    0.2301,    0.4458,    0.1266,   -0.0017,   -0.1713,
+      -0.0089,    0.1006,   14.5461,    0.1070,   -0.0016,    0.3206,
+      0.0102,   -0.1144,   13.9227,    0.8343,    0.0004,   2-0.3244;
  
     Eigen::MatrixXd PWMmatrix(4,1);
 
-      // PWM matrix needs to be multiplied to get the PWM value, since its in terms of torque rightnow
+    // PWM matrix needs to be multiplied to get the PWM value, since its in terms of torque rightnow
+
+      //pwm = ((-k(IMU velocities)-k(intergral(y-x_desired)))*(thrust_to_pwm_const)
     PWMmatrix = K*(MatrixDesiredvelocity- MatrixCurrentvelocity); // - ki(velocity;accelaration matrix)
 
     // set this to the frequency of the controller
@@ -92,16 +114,16 @@ public:
   }
 
 };
-  int main(int argc, char **argv){
+int main(int argc, char **argv){
 
-    //creates the node name something to do with command line
-    ros::init(argc, argv, "motor_controller_node");
-    ControllerNode Controller_Node (ros::this_node::getName());
+  //creates the node name something to do with command line
+  ros::init(argc, argv, "motor_controller_node");
+  ControllerNode Controller_Node (ros::this_node::getName());
 
 
-    ros::spin();    
-    return 0;
+  ros::spin();    
+  return 0;
 
-  }
+}
 
 
