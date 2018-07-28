86d579dad9fc9858242420eed87284bc79f3272b
Joel <joel.ahn@alumni.ubc.ca>
2018-07-28 13:21:06 -0700

    moved motor_control to branch

--- src/motor_control/motor_control.ino
+++ src/motor_control/motor_control.ino
@@ -1,17 +1,20 @@
 #include <ros.h>
-#include <std_msgs/Int32MultiArray.h>
+#include <std_msgs/Int16MultiArray.h>
 #include <Servo.h>
 
 /***********************/
 /*        Servo        */
 /***********************/
 
-// Define the motor ESC pins
-#define THRUSTER_LEFT_STR  2
-#define THRUSTER_LEFT_ANG  4
-#define THRUSTER_RIGHT_STR 5
-#define THRUSTER_RIGHT_ANG 3
+//Define the motor ESC pins
+#define THRUSTER_LEFT_STR  3
+#define THRUSTER_LEFT_ANG  5
+#define THRUSTER_RIGHT_STR 6
+#define THRUSTER_RIGHT_ANG 9
 
+//Define some ESC PWM calibration constants
+#define PWM_STOP        91
+#define PWM_STATIC_MOVE 50
 Servo TLS; //Thruster on Left pointing Straight
 Servo TLA; //Thruster on Left Angled 45
 Servo TRS; //Thruster on Right pointing Straight
@@ -45,14 +48,14 @@ ros::NodeHandle nh;
  * move in either yaw or linear x. The angled motors 
  * allow it to exclusively move in either linear y or z.
  */
-void motorControlCallback (const std_msgs::Int32MultiArray& msg){
+void motorControlCallback (const std_msgs::Int16MultiArray& msg){
   TRS.writeMicroseconds(msg.data[0]);
   TLS.writeMicroseconds(msg.data[1]);
   TRA.writeMicroseconds(msg.data[2]);
   TLA.writeMicroseconds(msg.data[3]);
 }
 
-ros::Subscriber<std_msgs::Int32MultiArray> motor_arduino_node("/arduino", motorControlCallback);
+ros::Subscriber<std_msgs::Int16MultiArray> motor_arduino_node("Arduino", motorControlCallback);
 
 /***********************/
 /*       Arduino       */
@@ -79,3 +82,5 @@ void loop()  {
   }
   delay(1);
 }
+
+
