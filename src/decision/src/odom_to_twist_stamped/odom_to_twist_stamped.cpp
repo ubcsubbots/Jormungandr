920457c08b61f2d4e214ffd7b3d18d1c463f9a2e
Cameron Newton <newtoncameron@hotmail.com>
2018-09-13 08:28:19 -0600

    Added option to launch robot in simulator mode (#63)
    * Changed odom_to_twist_stamped to be a seperate node, added tests
    
    * Fixed test
    
    * Update OdomToTwistStamped.h
    
    * Update OdomToTwistStamped.cpp
    
    * Update odom_to_twist_stamped.cpp

---
+++ src/decision/src/odom_to_twist_stamped/odom_to_twist_stamped.cpp
@@ -0,0 +1,18 @@
+/*
+ * Created By: Cameron Newton
+ * Created On: Sept 12nd, 2018
+ * Description: A node that remaps the output of the decision node from an Odom
+ * message to a TwistStamped for the simulator
+ */
+
+#include <OdomToTwistStamped.h>
+
+int main(int argc, char** argv) {
+    std::string node_name = "odom_to_twist_stamped";
+
+    OdomToTwistStamped odomToTwistStamped(argc, argv, node_name);
+
+    ros::spin();
+
+    return 0;
+}
