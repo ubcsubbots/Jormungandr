0ea15489b9a5135d579968a4f8f6277211edd052
Cameron Newton <newtoncameron@hotmail.com>
2018-09-13 08:03:55 -0600

    Update odom_to_twist_stamped.cpp

--- src/decision/src/odom_to_twist_stamped/odom_to_twist_stamped.cpp
+++ src/decision/src/odom_to_twist_stamped/odom_to_twist_stamped.cpp
@@ -2,8 +2,7 @@
  * Created By: Cameron Newton
  * Created On: Sept 12nd, 2018
  * Description: A node that remaps the output of the decision node from an Odom
- * message
- *              to a TwistStamped for the simulator
+ * message to a TwistStamped for the simulator
  */
 
 #include <OdomToTwistStamped.h>
