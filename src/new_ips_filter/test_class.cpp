33790f463cb25af7e76ea19417d27ecca958c541
Cameron Newton <newtoncameron@hotmail.com>
2018-10-27 13:04:44 -0700

    New class in package

---
+++ src/new_ips_filter/test_class.cpp
@@ -0,0 +1,5 @@
+//
+// Created by da-cam on 27/10/18.
+//
+
+#include "test_class.h"
---
+++ src/new_ips_filter/test_class.h
@@ -0,0 +1,14 @@
+//
+// Created by da-cam on 27/10/18.
+//
+
+#ifndef PROJECT_TEST_CLASS_H
+#define PROJECT_TEST_CLASS_H
+
+
+class test_class {
+
+};
+
+
+#endif //PROJECT_TEST_CLASS_H
