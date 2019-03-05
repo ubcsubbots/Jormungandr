91fd02c5a038daa2335d06e34560a29c843b46c9
loganfillo <logan.fillo@gmail.com>
2019-03-04 18:30:54 -0800

    Run Test Cases script now properly kills the processes needed to run the simulation

--- src/simulator/run_test_cases.sh
+++ src/simulator/run_test_cases.sh
@@ -12,19 +12,25 @@ ARGS_CSV="$CURR_DIR/args.csv"
 # For each line in args.csv, initialize a test case with args and run the simulation
 while IFS="," read a1 a2 a3 a4 a5 a6 a7 a8
 do
-  echo ========================================
-  echo " TEST:$a1 ARGS:$a2,$a3,$a4,$a5,$a6,$a7"
-  echo ========================================
+  echo " ========================================"
+  echo " TEST: $a1 ARGS: $a2 $a3 $a4 $a5 $a6 $a7 "
+  echo " ========================================"
   ./init_test_case.sh $a1 $a2 $a3 $a4 $a5 $a6 $a7
   echo " RUNNING SIMULATION..."
-  #./run_simulator.sh "gate/gate1.xml &"
-  # Keep track of process which is running the simulator
+  ./run_simulator.sh "gate/gate.xml" &
   # Launch node which publishes messages to simulation robot in different process
-  # Keep track of process which is running the node
   echo " RUNNING TEST..."
-  # Run C++ script which checks to see if the robot passes through gate in this process
-  # Set variable to be the output of C++ script, which is written to once test ends
-  # Kill the process which is running the simulation and the process which is running the node
+  # Run C++ script which checks to see if the robot passes through gate
+  # Set a shell variable to be the output of C++ script, which is written to once test ends
+
+  # This is here to emulate the time it takes for the test to run. Because we are running the C++
+  # script in this process, the pkill commands will not execute until the C++ script finishes execution
+  sleep 20s
+
+  # Kill the script that runs the simulator, and the UWsim script
+  pkill -x run_simulator.s
+  pkill -x uwsim_binary
+  # Kill the process which is running the node
   echo " TEST COMPLETE!"
   # Write output of C++ script to a results.txt file
 
