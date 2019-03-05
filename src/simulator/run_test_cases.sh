ff809b6820aa7c1ed09f702e2cef62cc755fbbc4
loganfillo <logan.fillo@gmail.com>
2019-03-04 18:52:18 -0800

    Cleaned up terminal output in run test cases

--- src/simulator/run_test_cases.sh
+++ src/simulator/run_test_cases.sh
@@ -12,12 +12,13 @@ ARGS_CSV="$CURR_DIR/args.csv"
 # For each line in args.csv, initialize a test case with args and run the simulation
 while IFS="," read a1 a2 a3 a4 a5 a6 a7 a8
 do
+  echo
   echo " ========================================"
   echo " TEST: $a1 ARGS: $a2 $a3 $a4 $a5 $a6 $a7 "
   echo " ========================================"
   ./init_test_case.sh $a1 $a2 $a3 $a4 $a5 $a6 $a7
   echo " RUNNING SIMULATION..."
-  ./run_simulator.sh "gate/gate.xml" &
+  ./run_simulator.sh "gate/gate.xml" > /dev/null 2>&1 &
   # Launch node which publishes messages to simulation robot in different process
   echo " RUNNING TEST..."
   # Run C++ script which checks to see if the robot passes through gate
@@ -29,9 +30,10 @@ do
 
   # Kill the script that runs the simulator, and the UWsim script
   pkill -x run_simulator.s
-  pkill -x uwsim_binary
+  pkill -x uwsim_binary 
   # Kill the process which is running the node
   echo " TEST COMPLETE!"
+
   # Write output of C++ script to a results.txt file
 
 done < $ARGS_CSV
