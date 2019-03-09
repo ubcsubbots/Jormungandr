9c7043bb7c178531de057a3944b934323dd3ab0f
loganfillo <logan.fillo@gmail.com>
2019-03-09 13:55:09 -0800

    Preparing for merge

--- src/simulator/include/GateTestResultNode.h
+++ src/simulator/include/GateTestResultNode.h
@@ -3,6 +3,34 @@
  * Created On: March 9th, 2019
  * Description: Checks incoming data from simulation robot (girona500)
  *              and determines whether or not it has made it through
-                the  gate within a specified amount of time
+ *              the  gate within a specified amount of time
  *
  */
+
+#ifndef JORMUNGANDR_GATETESTRESULTNODE_H
+#define JORMUNGANDR_GATETESTRESULTNODE_H
+
+#include <stdout>
+#include <ros/ros.h>
+
+/* ROS msg types */
+#include <nav_msgs/Odometry.h>
+
+class GateTestResultNode {
+  public:
+    GateTestResultNode(int argc, char** argv, std::string node_name);
+
+  private:
+    nav_msgs::Odometry::ConstPtr& currMessage;
+
+    /**
+     * Callback function for when data is received from girona500
+     *
+     * @param odometry discretized messages
+     */
+     void odometryCallBack(const nav_msgs::Odometry::ConstPtr& msg);
+
+
+};
+
+ #endif //JORMUNGANDR_GATETESTRESULTNODE_H
--- src/simulator/src/GateTestResultNode.cpp
+++ src/simulator/src/GateTestResultNode.cpp
@@ -7,79 +7,21 @@
  *
  */
 
-#include <stdout>
-#include <ros/ros.h>
-#include <nav_msgs/Odometry.h>
+GateTestResultNode::GateTestResultNodeint argc, char** argv, std::string node_name) {
+  // Setup Nodehandle
+  ros::init(argc, argv, "test result");
+  ros::NodeHandle nh;
 
-struct OdomMessage
-{
-  // Pose with covariance
-  float pos_x;
-  float pos_y;
-  float pos_z;
-  float quat_x;
-  float quat_y;
-  float quat_z;
-  float quat_w
-  float pose_cov[36];
-
-  // Twist with covariance
-  float linear_x;
-  float linear_y;
-  float linear_z;
-  float angular_x;
-  float angular_y;
-  float angular_z;
-  float twist_cov[36]
-
-};
-
-struct OdomMessage currMessage;
-
-void odometryCallBack(const nav_msgs::Odometry::ConstPtr& msg)
-{
-  ROS_INFO("[HEARD]: ODOMETRY_MESSAGE");
-
-  // Setup currMessage
-  currMessage.pos_x      = msg->pose.pose.position.x;
-  currMessage.pos_y      = msg->pose.pose.position.y;
-  currMessage.pos_z      = msg->pose.pose.position.z;
-  currMessage.quat_x     = msg->pose.pose.orientation.x;
-  currMessage.quat_y     = msg->pose.pose.orientation.y;
-  currMessage.quat_z     = msg->pose.pose.orientation.z;
-  currMessage.pose_cov   = msg->pose.covariance;
-
-  currMessage.linear_x   = msg->twist.twist.linear.x;
-  currMessage.linear_x   = msg->twist.twist.linear.y;
-  currMessage.linear_x   = msg->twist.twist.linear.z;
-  currMessage.angular_x  = msg->twist.twist.angular.x;
-  currMessage.angular_x  = msg->twist.twist.angular.y;
-  currMessage.angular_x  = msg->twist.twist.angular.z;
-  currMessage.twist_cov  = msg->twist.covariance;
+  // Setup subscriber
+  std::string topic = "/uwsim/g500_odom";
+  int refresh_rate  = 10;
+  ros::Subscriber sim_sub = nh.subscribe(
+    topic, refresh_rate, &odometryCallBack);
 }
 
-int main(int argc, char** argv)
-{
-    // Setup Nodehandle
-    ros::init(argc, argv, "test result");
-    ros::NodeHandle nh;
+void GateTestResultNode::odometryCallBack(
+  const nav_msgs::Odometry::ConstPtr& msg){
 
-    // Setup subscriber
-    std::string topic = "/uwsim/g500_odom";
-    int refresh_rate  = 10;
-    ros::Subscriber sim_sub = nh.subscribe(
-      topic, refresh_rate, &odometryCallBack);
-
-    ros::Rate loop_rate = 1;
-    int count = 0;
-    while (ros::ok()){
-      // Check to see if it has gone through the gate
-      if ()
-        std::cout<<"PASSED"<<std::endl;
-
-      if (count == 15) break;
-      count ++;
-
-    }
-    std::cout<<"FAILED"<<std::endl;
+  ROS_INFO("[HEARD]: ODOMETRY_MESSAGE");
+  currMessage = msg;
 }
