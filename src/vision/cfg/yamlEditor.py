e7330cfd886cfbdea25569e38f0a060edda898b4
Bryson Marazzi <bryson.marazzi@gmail.com>
2019-03-12 17:23:07 -0700

    Everything works! Implemented launch file to read the updated version of a yaml file outout from the python

--- src/vision/cfg/yamlEditor.py
+++ src/vision/cfg/yamlEditor.py
@@ -1,6 +1,49 @@
 #!/usr/bin/env python
 
 
+import yaml
+import io,sys
 
 
-print("\nHello World\n")
+'''
+	Method to parse python objects from a given yaml file, and output a cleaner version of the parameters to outFileName. 
+	Expects, outFileName and inFileName to have extension .yaml or .yml. 
+'''
+def edit(inFileName,outFileName):
+	#get the bit strea from the yaml file
+	stream = file(inFileName, 'r')    # 'document.yaml' contains a single YAML document.
+
+	#get the python object from the yaml file
+	object = yaml.load(stream)
+
+	#obtain the keys that are represented in the yaml file
+	keys = object.keys()
+
+
+	#make a dictinary to write to yaml file, ignoring the "groups"
+	parameters = dict()
+	for k in keys:
+		if(k != "groups"):
+			parameters[k] = object.get(k)
+
+	#write to a new yaml file
+	with io.open(outFileName, 'w', encoding='utf8') as outfile:
+	    yaml.dump(parameters, outfile, default_flow_style=False, allow_unicode=True)
+
+
+def main():
+    #Obtain the parameters passed from the 
+    if len(sys.argv) > 2:
+	    inFileName = sys.argv[1]
+	    outFileName = sys.argv[2]
+	    #print("\n\nOUTFILENAME\n\n" + outFileName)
+	    edit(inFileName,outFileName)
+	    print("Success, output yaml file as " + outFileName)
+
+    else:
+	print("Error, please pass two parameters, inFile, outFile.")
+
+
+if __name__ == '__main__':
+    main()
+
