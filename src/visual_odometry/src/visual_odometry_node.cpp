f2eacdb6a1418e8b3ade7626703db1c90e0c41cf
Andrew Chan <chan.2019@alumni.ubc.ca>
2020-02-01 12:29:52 -0800

    Hello World

---
+++ src/visual_odometry/src/visual_odometry_node.cpp
@@ -0,0 +1,22 @@
+/*
+ * Created By: Andrew Chan
+ * Created On: January 25, 2020
+ * Description: A simple node 
+ */
+ 
+#include <ros/ros.h>
+
+int main(int argc, char** argv) {
+    ros::init(argc, argv, "visual_odometry_node");
+
+    std::string node_name = "visual_odometry_node";
+    ros::NodeHandle nh;
+
+    ROS_INFO("Hello, World!");
+
+    ros::spin();
+  
+    return 0;
+}
+
+
