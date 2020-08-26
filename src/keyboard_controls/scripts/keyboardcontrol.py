d9ed5c3114989b020f0681725d2303b0a9b2316b
PythonSyntax1 <51872355+PythonSyntax1@users.noreply.github.com>
2020-08-26 09:42:02 -0700

    Update keyboardcontrol.py
    Removed unnecessary inner while loop

--- src/keyboard_controls/scripts/keyboardcontrol.py
+++ src/keyboard_controls/scripts/keyboardcontrol.py
@@ -17,50 +17,46 @@ def main():
     rospy.init_node('keyboard_control', anonymous=True)
     while not rospy.is_shutdown():
         msg = Pose()
-        x = True
-        while x = True:
-            print("Enter [x,y,z,phi,theta,psi] with no spaces, such as [5,2,1,0,0,3.14]")
-            inputStr = input()
-            split = inputStr.split(",")
-            if (len(split) != 6):
-                continue
-            try:
-                msg.position.x = int(split[0])
-            except ValueError:
-                print("X Integer conversion error")
-                continue
-            try:
-                msg.position.y = int(split[1])
-            except ValueError:
-                print("Y Integer conversion error")
-                continue
-            try:
-                msg.position.z = int(split[2])
-            except ValueError:
-                print("Z Integer conversion error")
-                continue
-            try:
-                roll = int(split[3])
-            except ValueError:
-                print("Phi Integer conversion error")
-                continue
-            try:
-                pitch = int(split[4])
-            except ValueError:
-                print("Pitch Integer conversion error")
-                continue
-            try:
-                yaw = int(split[5])
-            except ValueError:
-                print("Yaw Integer conversion error")
-                continue
-            quaternion = tf.transformations.quaternion_from_euler(roll, pitch, yaw)
-            msg.orientation.x = quaternion[0]
-            msg.orientation.y = quaternion[1]
-            msg.orientation.z = quaternion[2]
-            msg.orientation.w = quaternion[3]
-
-            x = False
+        print("Enter [x,y,z,phi,theta,psi] with no spaces, such as [5,2,1,0,0,3.14]")
+        inputStr = input()
+        split = inputStr.strip('][').split(",")
+        if (len(split) != 6):
+            continue
+        try:
+            msg.position.x = int(split[0])
+        except ValueError:
+            print("X Integer conversion error")
+            continue
+        try:
+            msg.position.y = int(split[1])
+        except ValueError:
+            print("Y Integer conversion error")
+            continue
+        try:
+            msg.position.z = int(split[2])
+        except ValueError:
+            print("Z Integer conversion error")
+            continue
+        try:
+            roll = int(split[3])
+        except ValueError:
+            print("Phi Integer conversion error")
+            continue
+        try:
+            pitch = int(split[4])
+        except ValueError:
+            print("Pitch Integer conversion error")
+            continue
+        try:
+            yaw = int(split[5])
+        except ValueError:
+            print("Yaw Integer conversion error")
+            continue
+        quaternion = tf.transformations.quaternion_from_euler(roll, pitch, yaw)
+        msg.orientation.x = quaternion[0]
+        msg.orientation.y = quaternion[1]
+        msg.orientation.z = quaternion[2]
+        msg.orientation.w = quaternion[3]
         publisher.publish(msg)
         print("Message published")
 
